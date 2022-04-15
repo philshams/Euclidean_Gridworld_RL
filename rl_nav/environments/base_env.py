5ed057c30bdcdd23f1b63c5c549af4353f08beb5
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2022-04-15 19:20:54 +0100

    setup method and average over positional states method

--- rl_nav/environments/base_env.py
+++ rl_nav/environments/base_env.py
@@ -1,8 +1,11 @@
 import abc
-from typing import List, Tuple
+import itertools
+from typing import Dict, List, Optional, Tuple
 
 import numpy as np
 from rl_nav import constants
+from rl_nav.utils import env_utils
+
 
 class BaseEnvironment(abc.ABC):
     """Base class for RL environments.
@@ -22,6 +25,48 @@ class BaseEnvironment(abc.ABC):
         self._training: bool
         self._active: bool
         self._episode_step_count: int
+
+    def _setup_environment(self, map_ascii_path: Optional[str] = None):
+
+        if map_ascii_path is not None:
+            self._map = env_utils.parse_map_outline(
+                map_file_path=map_ascii_path, mapping=self.MAPPING
+            )
+
+        self._rewards = env_utils.setup_rewards(
+            self._reward_positions, self._reward_attributes
+        )
+        self._total_rewards = len(self._rewards)
+
+        (
+            self._positional_state_space,
+            self._rewards_received_state_space,
+            self._state_space,
+            self._wall_state_space,
+        ) = env_utils.configure_state_space(
+            map_outline=self._map,
+            reward_positions=self._reward_positions,
+        )
+
+    def average_values_over_positional_states(
+        self, values: Dict[Tuple[int], float]
+    ) -> Dict[Tuple[int], float]:
+        """For certain analyses (e.g. plotting value functions) we want to
+        average the values for each position over all non-positional state information--
+        in this case the key posessions.
+        Args:
+            values: full state-action value information
+        Returns:
+            averaged_values: (positional-state)-action values.
+        """
+        averaged_values = {}
+        for state in self._positional_state_space:
+            non_positional_set = [
+                v for k, v in values.items() if k[:2] == state
+            ]
+            non_positional_mean = np.mean(non_positional_set, axis=0)
+            averaged_values[state] = non_positional_mean
+        return averaged_values
 
     @abc.abstractmethod
     def step(self, action: int) -> Tuple[float, Tuple[int, int]]:
