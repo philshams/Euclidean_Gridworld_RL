c9def60e70c4112022044abb56d7b9df020c1866
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2022-04-21 16:33:39 +0100

    initial commit of separate q learning runner

---
+++ rl_nav/runners/q_learning_runner.py
@@ -0,0 +1,47 @@
+from rl_nav.runners import episodic_runner, lifelong_runner
+
+
+class LifelongQLearningRunner(lifelong_runner.LifelongRunner):
+    def __init__(self, config, unique_id: str):
+
+        super().__init__(config=config, unique_id=unique_id)
+
+    def _train_step(self, state) -> float:
+        """Perform single training step."""
+        action = self._model.select_behaviour_action(state, epsilon=self._epsilon)
+        reward, new_state = self._train_environment.step(action)
+
+        self._model.step(
+            state=state,
+            action=action,
+            reward=reward,
+            new_state=new_state,
+            active=self._train_environment.active,
+        )
+
+        self._step_count += 1
+
+        return new_state, reward
+
+
+class EpisodicQLearningRunner(episodic_runner.EpisodicRunner):
+    def __init__(self, config, unique_id: str):
+
+        super().__init__(config=config, unique_id=unique_id)
+
+    def _train_step(self, state) -> float:
+        """Perform single training step."""
+        action = self._model.select_behaviour_action(state, epsilon=self._epsilon)
+        reward, new_state = self._train_environment.step(action)
+
+        self._model.step(
+            state=state,
+            action=action,
+            reward=reward,
+            new_state=new_state,
+            active=self._train_environment.active,
+        )
+
+        self._step_count += 1
+
+        return new_state, reward
