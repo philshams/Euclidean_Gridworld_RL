be409a528b5de48f4250f9f5fae6c84ca7d6e22f
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2022-04-23 13:46:09 +0100

    testing, visualisations etc. on a step basis

--- rl_nav/runners/episodic_runner.py
+++ rl_nav/runners/episodic_runner.py
@@ -31,23 +31,7 @@ class EpisodicRunner(base_runner.BaseRunner):
 
     def train(self):
         while self._step_count < self._num_steps:
-            if self._step_count >= self._next_rollout_step:
-                self._train_rollout()
-            #     self._test_rollout(save_name_base=)
-            if self._step_count >= self._next_visualisation_step:
-                self._generate_visualisations()
-            if self._step_count >= self._next_test_step:
-                plain_logging_dict = self._test(self._model)
-                find_reward_logging_dict = self._find_reward_test()
-                test_logging_dict = {**plain_logging_dict, **find_reward_logging_dict}
-            else:
-                test_logging_dict = {}
-            episode_logging_dict = {**test_logging_dict, **self._train_episode()}
-            self._log_episode(step=self._step_count, logging_dict=episode_logging_dict)
-            if self._step_count >= self._next_checkpoint_step:
-                self._data_logger.checkpoint()
-                while self._next_checkpoint_step <= self._step_count:
-                    self._next_checkpoint_step += self._checkpoint_frequency
+            self._train_episode()
 
     def _train_episode(self) -> Dict[str, Any]:
         """Perform single training loop.
@@ -66,13 +50,15 @@ class EpisodicRunner(base_runner.BaseRunner):
 
         while self._train_environment.active and self._step_count < self._num_steps:
 
-            state, reward = self._train_step(state=state)
+            state, reward, logging_dict = self._train_step(state=state)
             episode_reward += reward
 
-        logging_dict = {
-            constants.STEP: self._step_count,
-            constants.TRAIN_EPISODE_REWARD: episode_reward,
-            constants.TRAIN_EPISODE_LENGTH: self._train_environment.episode_step_count,
-        }
+            logging_dict[constants.STEP] = self._step_count
 
-        return logging_dict
+            if not self._train_environment.active:
+                logging_dict[constants.TRAIN_EPISODE_REWARD] = episode_reward
+                logging_dict[
+                    constants.TRAIN_EPISODE_LENGTH
+                ] = self._train_environment.episode_step_count
+
+            self._log_episode(step=self._step_count, logging_dict=logging_dict)
