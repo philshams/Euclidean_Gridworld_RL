a21de19d560869b14c55e0235738b97482cb82ed
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2022-04-23 13:47:06 +0100

    logging organisation changed

--- rl_nav/runners/lifelong_runner.py
+++ rl_nav/runners/lifelong_runner.py
@@ -16,22 +16,8 @@ class LifelongRunner(base_runner.BaseRunner):
         train_reward = 0
 
         while self._step_count < self._num_steps:
-            # if self._step_count >= self._next_rollout_step:
-            #     self._test_rollout(save_name_base=constants.INDIVIDUAL_TEST_RUN)
-            if self._step_count >= self._next_visualisation_step:
-                self._generate_visualisations()
-            if self._step_count >= self._next_test_step:
-                plain_logging_dict = self._test(self._model)
-                find_reward_logging_dict = self._find_reward_test()
-                logging_dict = {**plain_logging_dict, **find_reward_logging_dict}
-            else:
-                logging_dict = {}
-            if self._step_count >= self._next_checkpoint_step:
-                self._data_logger.checkpoint()
-                while self._next_checkpoint_step <= self._step_count:
-                    self._next_checkpoint_step += self._checkpoint_frequency
 
-            state, reward = self._train_step(state=state)
+            state, reward, logging_dict = self._train_step(state=state)
             train_reward += reward
 
             logging_dict[constants.STEP] = self._step_count
