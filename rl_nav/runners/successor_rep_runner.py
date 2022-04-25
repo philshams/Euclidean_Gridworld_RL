78955c7c01bbcdc5d2660c661155f416c672e98b
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2022-04-25 19:09:59 +0100

    trian step methods imlemented

--- rl_nav/runners/successor_rep_runner.py
+++ rl_nav/runners/successor_rep_runner.py
@@ -6,9 +6,20 @@ class LifelongSRRunner(lifelong_runner.LifelongRunner):
 
         super().__init__(config=config, unique_id=unique_id)
 
-    def _train_step(self, state) -> float:
+    def _model_train_step(self, state) -> float:
         """Perform single training step."""
-        pass
+        action = self._model.select_behaviour_action(state, epsilon=self._epsilon)
+        reward, new_state = self._train_environment.step(action)
+
+        self._model.step(
+            state=state,
+            action=action,
+            reward=reward,
+            new_state=new_state,
+            active=self._train_environment.active,
+        )
+
+        return new_state, reward
 
 
 class EpisodicSRRunner(episodic_runner.EpisodicRunner):
@@ -16,6 +27,17 @@ class EpisodicSRRunner(episodic_runner.EpisodicRunner):
 
         super().__init__(config=config, unique_id=unique_id)
 
-    def _train_step(self, state) -> float:
+    def _model_train_step(self, state) -> float:
         """Perform single training step."""
-        pass
+        action = self._model.select_behaviour_action(state, epsilon=self._epsilon)
+        reward, new_state = self._train_environment.step(action)
+
+        self._model.step(
+            state=state,
+            action=action,
+            reward=reward,
+            new_state=new_state,
+            active=self._train_environment.active,
+        )
+
+        return new_state, reward
