19a2b913d1b8a27b55fb2b8ba5db184f85ceb39c
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2022-04-26 16:42:17 +0100

    suite of sr

---
+++ rl_nav/experiments/suites/successor_rep/config_changes.py
@@ -0,0 +1,66 @@
+CONFIG_CHANGES = {
+    "condition_1_2": [
+        {
+            "train_environment": {"map_path": "../maps/square_escape_map.txt"},
+            "test_environments": {
+                "map_paths": [
+                    "../maps/square_escape_test_map.txt",
+                    "../maps/square_escape_map.txt",
+                ]
+            },
+        }
+    ],
+    "condition_3": [
+        {
+            "train_environment": {
+                "map_path": "../maps/empty_square_map.txt",
+                "reward_positions": [],
+            },
+            "test_environments": {
+                "map_paths": [
+                    "../maps/square_escape_test_map.txt",
+                    "../maps/square_escape_map.txt",
+                ]
+            },
+        }
+    ],
+    "condition_4": [
+        {
+            "train_environment": {
+                "map_path": "../maps/vector_blocked_square_escape_map.txt"
+            },
+            "test_environments": {
+                "map_paths": [
+                    "../maps/square_escape_test_map.txt",
+                    "../maps/square_escape_map.txt",
+                ]
+            },
+        }
+    ],
+    "condition_5": [
+        {
+            "train_environment": {
+                "map_path": "../maps/shelter_blocked_square_escape_map.txt"
+            },
+            "test_environments": {
+                "map_paths": [
+                    "../maps/square_escape_test_map.txt",
+                    "../maps/square_escape_map.txt",
+                ]
+            },
+        }
+    ],
+    "condition_6": [
+        {
+            "train_environment": {
+                "map_path": "../maps/vector_threat_only_blocked_square_escape_map.txt"
+            },
+            "test_environments": {
+                "map_paths": [
+                    "../maps/square_escape_test_map.txt",
+                    "../maps/square_escape_map.txt",
+                ]
+            },
+        }
+    ],
+}
