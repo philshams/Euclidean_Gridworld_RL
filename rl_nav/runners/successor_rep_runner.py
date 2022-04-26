dcb6c66cd858126fc48464ef57f4dc6a27d8627e
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2022-04-26 09:38:07 +0100

    imeplemented reward position and placefield plots

--- rl_nav/runners/successor_rep_runner.py
+++ rl_nav/runners/successor_rep_runner.py
@@ -1,3 +1,6 @@
+import os
+
+from rl_nav import constants
 from rl_nav.runners import episodic_runner, lifelong_runner
 
 
@@ -21,6 +24,27 @@ class LifelongSRRunner(lifelong_runner.LifelongRunner):
 
         return new_state, reward
 
+    def _runner_specific_visualisations(self):
+        # reward function visualisation
+        self._train_environment.plot_heatmap_over_env(
+            heatmap=self._model.reward_function,
+            save_name=os.path.join(
+                self._visualisations_folder_path,
+                f"{self._step_count}_{constants.SR_REWARD_FUNCTION_PDF}",
+            ),
+        )
+        # place field of reward state
+        for i, reward_pos in enumerate(self._train_environment.reward_positions):
+            place_field = self._model.get_place_field(state=reward_pos)
+
+            self._train_environment.plot_heatmap_over_env(
+                heatmap=place_field,
+                save_name=os.path.join(
+                    self._visualisations_folder_path,
+                    f"{self._step_count}_{i}_{constants.SR_REWARD_POS_PLACE_FIELD_PDF}",
+                ),
+            )
+
 
 class EpisodicSRRunner(episodic_runner.EpisodicRunner):
     def __init__(self, config, unique_id: str):
@@ -41,3 +65,12 @@ class EpisodicSRRunner(episodic_runner.EpisodicRunner):
         )
 
         return new_state, reward
+
+    def _runner_specific_visualisations(self):
+        self._train_environment.plot_heatmap_over_env(
+            heatmap=averaged_max_values,
+            save_name=os.path.join(
+                self._visualisations_folder_path,
+                f"{self._step_count}_{constants.VALUES_PDF}",
+            ),
+        )
