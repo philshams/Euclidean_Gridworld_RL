bafe2e6897248beb3da1b30301cdf3e0c637afce
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2022-06-08 14:06:45 +0100

    moving into d blcok logic

--- rl_nav/environments/escape_env_diagonal.py
+++ rl_nav/environments/escape_env_diagonal.py
@@ -139,6 +139,16 @@ class EscapeEnvDiagonal(escape_env.EscapeEnv):
             )
         )
 
+        moving_into_d_block = (
+            (self._d_block_state_space is not None)
+            and (tuple(provisional_new_position) in self._d_block_state_space)
+            and (
+                np.array_equal(delta, self.DELTAS[3])
+                or np.array_equal(delta, self.DELTAS[6])
+                or np.array_equal(delta, self.DELTAS[7])
+            )
+        )
+
         moving_into_b_block = (
             (self._b_block_state_space is not None)
             and (tuple(provisional_new_position) in self._b_block_state_space)
@@ -168,6 +178,7 @@ class EscapeEnvDiagonal(escape_env.EscapeEnv):
                 not moving_into_wall,
                 not moving_into_k_block,
                 not moving_into_h_block,
+                not moving_into_d_block,
                 not moving_into_b_block,
                 not moving_from_b_block,
                 not moving_into_c_block,
