7e671dbcd7a3ec132b08ce8fc203b5cd0dce984b
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2022-06-23 22:56:14 +0100

    epsilon value property call

--- rl_nav/runners/a_star_runner.py
+++ rl_nav/runners/a_star_runner.py
@@ -42,7 +42,7 @@ class EpisodicAStarRunner(episodic_runner.EpisodicRunner):
 
     def _model_train_step(self, state) -> float:
         """Perform single training step."""
-        action = self._model.select_behaviour_action(state, epsilon=self._epsilon)
+        action = self._model.select_behaviour_action(state, epsilon=self._epsilon.value)
         reward, new_state = self._train_environment.step(action)
 
         self._model.step(
