ee56856b7629597b57e3381edb5053284fd5c94a
Philip Shamash <philshams@ucla.edu>
2022-09-25 12:09:12 -0400

    clean up

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ import setuptools
 
 setuptools.setup(
     name='rl-nav-initialization',
-    version='0.0.1',
-    author='Philip Shamash',
+    version='0.1.2',
+    author='Philip Shamash and Sebastian Lee',
     license='GNU General Public License',
     packages = ['rl_nav'],
     package_dir={'rl_nav': 'rl_nav'}
