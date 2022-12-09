a3104f25a6a7c04b35383eaf62842557551b0fbf
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2022-12-09 13:17:41 +0000

    remove old files

--- rl_nav/demo.py
+++
@@ -1,7 +0,0 @@
-from rl_nav.model import Euclidean_Gridworld_RL
-from rl_nav.config import timestep_to_display
-
-if __name__=='__main__':       
-    simulation = Euclidean_Gridworld_RL()
-    print(f" - Running one experiment and displaying the test episode after {timestep_to_display} training steps")
-    simulation.model_free_experiment(display_an_episode=True)
